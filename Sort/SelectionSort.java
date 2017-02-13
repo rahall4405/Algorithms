932aa4edc8b0f8e54c71e7969c6632f8159f6f50
Robert Hall <rahall4405@gmail.com>
2017-02-12 23:05:42 -0600

    Add - initial SelectionSort Class

---
+++ Sort/SelectionSort.java
@@ -0,0 +1,15 @@
+package rahall.example.com.bubblesort;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rahall4405 on 2/12/17.
+ */
+
+public class SelectionSort {
+    private ArrayList<Integer> intArray;
+    private long sortTime =0;
+    public SelectionSort(ArrayList<Integer> intArray) {
+        this.intArray = intArray;
+    }
+}
