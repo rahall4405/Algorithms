64b0fc9e9d9d7c4019a37f07c9125e989727e506
Robert Hall <rahall4405@gmail.com>
2017-02-13 14:21:45 -0600

    add - added SelectionSort implementation

--- Sort/SelectionSort.java
+++ Sort/SelectionSort.java
@@ -8,8 +8,44 @@ import java.util.ArrayList;
 
 public class SelectionSort {
     private ArrayList<Integer> intArray;
-    private long sortTime =0;
+    private long sortTime = 0;
+
     public SelectionSort(ArrayList<Integer> intArray) {
         this.intArray = intArray;
     }
+
+    public ArrayList<Integer> sortInteger() {
+        int sortedIndex = 0;
+        boolean inOrder = false;
+        int smallestNumberIndex = 0;
+        long startTime = System.currentTimeMillis();
+        do {
+            for (int i = 0 + sortedIndex; i < intArray.size(); i++) {
+                if (i == 0) {
+                    smallestNumberIndex = sortedIndex;
+
+                } else if (i == intArray.size() - 1) {
+                    if (intArray.get(i) < intArray.get(smallestNumberIndex)) {
+                        smallestNumberIndex = i;
+                    }
+                    int tempInt = intArray.get(smallestNumberIndex);
+                    intArray.set(smallestNumberIndex, intArray.get(sortedIndex));
+                    intArray.set(sortedIndex, tempInt);
+                    sortedIndex++;
+                } else {
+                    if (intArray.get(i) < intArray.get(smallestNumberIndex)) {
+                        smallestNumberIndex = i;
+                    }
+                }
+
+            }
+        } while (sortedIndex < intArray.size() - 1);
+        sortTime = (long) System.currentTimeMillis() - startTime;
+        return intArray;
+
+    }
+    public long getSortTime() {
+        return sortTime;
+    }
+
 }
