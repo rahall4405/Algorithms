ae134d02b00efc4034930545efdc465ba36678ff
Robert Hall <rahall4405@gmail.com>
2017-02-14 22:45:38 -0600

    add - add initial InsertionSort code

---
+++ Sort/InsertionSort.java
@@ -0,0 +1,16 @@
+package rahall.example.com.bubblesort;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rahall4405 on 2/14/17.
+ */
+
+public class InsertionSort {
+    private ArrayList<Integer> intArray;
+    private long sortTime =0;
+
+    public InsertionSort(ArrayList<Integer> intArray) {
+        this.intArray = intArray;
+    }
+}
