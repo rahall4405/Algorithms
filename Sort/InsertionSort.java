2d029079ead38d3a6d5b77370a2d44e49d8102f5
Robert Hall <rahall4405@gmail.com>
2017-02-16 00:19:16 -0600

    add - Insertion sort completed code

--- Sort/InsertionSort.java
+++ Sort/InsertionSort.java
@@ -12,5 +12,34 @@ public class InsertionSort {
 
     public InsertionSort(ArrayList<Integer> intArray) {
         this.intArray = intArray;
+
     }
+
+    public ArrayList<Integer> sortInteger() {
+        boolean inOrder = false;
+        long startTime = System.currentTimeMillis();
+        int sortPosition = 0;
+        do {
+            for (int i=sortPosition; i>= 0; i--) {
+                if (intArray.get(i) > intArray.get(i+1)) {
+                    int tempInt = intArray.get(i);
+                    intArray.set(i,intArray.get(i+1));
+                    intArray.set(i+1,tempInt);
+                }
+
+                if(sortPosition == intArray.size()-2) {
+
+                    inOrder = true;
+                }
+            }
+            sortPosition++;
+        } while (inOrder == false);
+        sortTime = (long) System.currentTimeMillis() - startTime;
+        return intArray;
+
+    }
+    public long getSortTime() {
+        return sortTime;
+    }
+
 }
